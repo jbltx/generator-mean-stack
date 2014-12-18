8f4a7b4d386d3d0432cd21d68acdefcd8596c57a
Jambonlatex <gtmicka@hotmail.fr>
2014-12-18 04:16:48 +0100

    Create _routes.js

---
+++ app/templates/_app/_backend/_router/_routes.js
@@ -0,0 +1,80 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var express = require('express');
+var router = express.Router();
+var Model= mongoose.model('Model');
+
+module.exports = function (isLoggedIn) {
+
+	router.get('/', isLoggedIn, function (req, res) {
+		Model.listAll(req.user, function (err, data) {
+			if (err) {
+				return res.status(500).send(err);
+			}
+			if (data.length === 0) {
+				return res.status(404).end();
+			}
+			return res.status(200).send(data);
+		});
+	});
+
+	router.post('/', isLoggedIn, function (req, res) {
+		var newData = new Model();
+		newData.createDoc(req.body, function (err) {
+			if(err) {
+				return res.status(500).send(err);
+			}
+			return res.status(200).send(newData);
+		});
+	});
+
+	router.get('/:id', isLoggedIn, function (req, res) {
+		Model.findById(req.params.id, function (err, data) {
+			if (err) {
+				return res.status(500).send(err);
+			}
+			if (!data) {
+				return res.status(404).end();
+			}
+			return res.status(200).send(data);
+		});
+	});
+
+	router.put('/:id', isLoggedIn, function (req, res) {
+		Model.findById(req.params.id, function (err, data) {
+			if (err) {
+				return res.status(500).send(err);
+			}
+			if (!data) {
+				return res.status(404).end();
+			}
+			data.updateDoc(req.body, function (err) {
+				if (err) {
+					return res.status(500).send(err);
+				}
+				return res.status(200).send(data);
+			});
+		});
+	});	
+
+	router.delete('/:id', isLoggedIn, function (req, res) {
+		Model.findById(req.params.id, function (err, data) {
+			if (err) {
+				return res.status(500).send(err);
+			}
+			if (!data) {
+				return res.status(404).end();
+			}
+			data.remove(function (err) {
+				if (err) {
+					return res.status(500).send(err);
+				}
+				return res.status(200).end();
+			});				
+		});
+	});	
+
+	return router;
+
+};
