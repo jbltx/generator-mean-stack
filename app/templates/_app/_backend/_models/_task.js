2f61d94eaf529f9bbe33a6155e12caa8420496d9
Jambonlatex <gtmicka@hotmail.fr>
2014-12-18 04:24:07 +0100

    Update _task.js

--- app/templates/_app/_backend/_models/_task.js
+++ app/templates/_app/_backend/_models/_task.js
@@ -7,4 +7,16 @@ var taskSchema = new Schema({
 	name: {type: String, required: true}
 });
 
+
+taskSchema.statics = {
+	fillDoc: function (document, bodyReq, callback) {
+		for (var field in this.schema.paths) {
+			if(field !== '_id' && field !== '_v' && bodyReq[field] !== undefined) {
+				document[field] = bodyReq[field];
+			}
+		}
+		document.save(callback);
+	}
+}
+
 mongoose.model('Task', taskSchema);
