f5fa455efe00a7c779b1500657b9c1aa61ae916c
Jambonlatex <gtmicka@hotmail.fr>
2014-12-18 03:38:02 +0100

    Create _index.js

---
+++ app/templates/_app/_backend/_router/_index.js
@@ -0,0 +1,86 @@
+'use strict';
+
+
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var Remember = mongoose.model('Remember');
+
+
+var isLoggedIn = function (req, res, next) {
+	if (!req.isAuthenticated()) {
+		res.send(401);
+	}
+	else {
+		next();
+	}
+};
+
+
+module.exports = function (app, passport) {
+
+	app.use('/task', require('./routes/task')(isLoggedIn));
+	
+	app.post('/signup', passport.authenticate('local-signup', {failureFlash : true}), function (req, res) {
+		res.send(req.user);
+	});
+
+
+	app.post('/signin', passport.authenticate('local-signin', {failureFlash : true}), function (req, res) {
+		return Remember.findOne({login: req.user.email})
+			.exec(function (err, data) {
+				if (err) {return res.status(500).send(err);}
+				if (data) {res.cookie('remember', data.login+'#'+data.serial_id+data.token, { maxAge: 360*24*3600000 });}
+				return res.send(req.user);
+			});
+	});
+
+
+	app.get('/signedin', function (req, res) {
+		res.send(req.isAuthenticated() ? req.user : '0');
+	});
+
+
+	app.post('/signout', function (req, res) {
+		req.logOut();
+		res.send(200);
+	});
+
+	app.post('/validate', function (req, res) {
+		User.findOne({ emailKey: req.body.key }, function (err, user) {
+			if (err || !user) { return res.status(404).end(); }
+			if (user.emailConfirm === true) {
+				return res.status(403).end();
+			}
+			else {
+				User.update({ emailKey: req.body.key }, { emailConfirm: true }, { multi: true }, function (err, nb) {
+					if (err) { return res.statut(404).end(); }
+					if (nb === 1) {
+						res.status(200).end();
+					}
+					else {
+						res.status(500).end();
+					}
+				});
+			}
+		});
+				
+	});
+
+	
+	app.post('/unvalidate', function (req, res) {
+		User.findOne({emailKey: req.body.key, email: req.body.email}, function (err, user) {
+			if (err || !user) { return res.status(404).end(); }
+			if (user.emailConfirm === true) {
+				return res.status(403).end();
+			}
+			else {
+				User.remove({ _id: user._id}, function (err) {
+					if (err) { return res.status(404).end(); }
+					res.status(200).end();
+				});
+			}
+		});
+	});
+
+
+};
